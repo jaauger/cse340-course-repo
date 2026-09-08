828c4f9b767defa142adbd749815f186cdefa5f2
jaauger <jaauger@yahoo.com>
2026-09-07 19:32:18 -0600

    W02 Learning activities push

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -34,12 +36,12 @@ app.get('/', async (req, res) => {
   const title = 'Home';
   res.render('home', { title });
 });
-
+/*
 app.get('/organizations', async (req, res) => {
   const title = 'Our Partner Organizations';
   res.render('organizations', { title });
 });
-
+*/
 app.get('/projects', async (req, res) => {
   const title = 'Service Projects';
   res.render('projects', { title });
@@ -49,8 +51,26 @@ app.get('/categories', async (req, res) => {
   const title = 'Service Project Categories';
   res.render('categories', { title });
 });
-
+/*
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
 });
+*/
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+});
+
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+    
+    res.render('organizations', { title, organizations });
+});
+
